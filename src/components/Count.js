b8dff82014937f1c415bf14c43f75e04728af512
nazmulhuda1 <nazmulhuda3130@gmail.com>
2023-02-20 09:42:43 +0600

    'react-counter-app'

---
+++ src/components/Count.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Count({ count }) {
+    return (
+        <div className="text-2xl font-semibold">{count}</div>
+    )
+}
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,32 +1,15 @@
-import { useState } from "react";
+import Button from "./Button";
+import Count from "./Count";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
+export default function Counter({ increment, decrement, id, count }) {
 
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
-
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div className="text-2xl font-semibold">{count}</div>
+            <Count count={count} />
             <div className="flex space-x-3">
-                <button
-                    className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={increment}
-                >
-                    Increment
-                </button>
-                <button
-                    className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrement}
-                >
-                    Decrement
-                </button>
+                <Button handler={() => increment(id)}>Increment</Button>
+                <Button handler={() => decrement(id)} >Decrement</Button>
             </div>
         </div>
     );
